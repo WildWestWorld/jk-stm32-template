a58f57d3e3813189dfcf44a5a6d965ed8cb620b7
ljk <380686356@qq.com>
2023-11-06 19:24:30 +0800

    添加Led Hardware工具类

---
+++ Module/Hardware/LED.h
@@ -0,0 +1,15 @@
+//如果没有定义 __LED_H 就定义__LED_H变量 以防重复定义
+#ifndef __LED_H
+#define __LED_H
+
+void LED_Init(void);
+
+void LED1_ON(void);
+void LED1_OFF(void);
+
+void LED2_ON(void);
+void LED2_OFF(void);
+
+
+#endif
+	
--- Objects/startup_stm32f10x_md.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f10x_md.o: module\start\startup_stm32f10x_md.s
